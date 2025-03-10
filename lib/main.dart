7e318d4a65a81828d076fa78197eda5e49ae448b
adityapal08 <bringerlight48@gmail.com>
2025-03-10 22:19:57 +0530

    updated

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ))),
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.onPrimary,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          )),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 16),
+              )),
+      home: Expenses()));
+}
